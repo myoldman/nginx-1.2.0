b58452cdb151e36bd1ad88a6007e107b7760d398
liuhong <myoldman@163.com>
2012-08-30 15:42:07 +0800

    test

--- src/http/ngx_http_write_filter_module.c
+++ src/http/ngx_http_write_filter_module.c
@@ -174,7 +174,7 @@ ngx_http_write_filter(ngx_http_request_t *r, ngx_chain_t *in)
                        cl->buf->start, cl->buf->pos,
                        cl->buf->last - cl->buf->pos,
                        cl->buf->file_pos,
-                       cl->buf->file_last - cl->buf->file_pos, cl->buf->start);
+                       cl->buf->file_last - cl->buf->file_pos, cl->buf->pos);
 	}
     clcf = ngx_http_get_module_loc_conf(r, ngx_http_core_module);
 
