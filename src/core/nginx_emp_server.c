5d52ba111da1b5a596d96367498de066fda075da
liuhong <myoldman@163.com>
2012-09-24 11:01:43 +0800

    test

--- src/core/nginx_emp_server.c
+++ src/core/nginx_emp_server.c
@@ -398,7 +398,8 @@ void request_callback(struct evhttp_request *req, void *arg)
 	        const char * result = evhttp_find_header(req->input_headers,"result");
 			if(result == NULL) {
 				printf("result is null\r\n");
-				event_base_loopexit(ctx->base, 0); 
+				ctx->ok = 0;
+				event_base_loopexit(ctx->base, 0);
 	        	break;
 			}
 			
