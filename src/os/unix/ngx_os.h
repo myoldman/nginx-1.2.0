a5ff4090a61abb8758a2a38fba069b4da0dc97da
ffcs <myoldman@163.com>
2012-11-15 15:47:49 +0800

    test

--- src/os/unix/ngx_os.h
+++ src/os/unix/ngx_os.h
@@ -45,7 +45,7 @@ ssize_t ngx_readv_chain(ngx_connection_t *c, ngx_chain_t *entry);
 ssize_t ngx_udp_unix_recv(ngx_connection_t *c, u_char *buf, size_t size);
 ssize_t ngx_unix_send(ngx_connection_t *c, u_char *buf, size_t size);
 ngx_chain_t *ngx_writev_chain(ngx_connection_t *c, ngx_chain_t *in,
-    off_t limit);
+    off_t limit, int is_gzip);
 
 #if (NGX_HAVE_AIO)
 ssize_t ngx_aio_read(ngx_connection_t *c, u_char *buf, size_t size);
