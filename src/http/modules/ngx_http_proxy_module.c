e98aa8ffd7d22b63c4b003c13bf29b3ec30b52a2
ffcs <myoldman@163.com>
2012-11-16 17:08:36 +0800

    test

--- src/http/modules/ngx_http_proxy_module.c
+++ src/http/modules/ngx_http_proxy_module.c
@@ -659,7 +659,6 @@ ngx_http_proxy_handler(ngx_http_request_t *r)
 	}
 
 	if (strlen(appid) == 0) {
-		printf("query string is %s\n" , r->args.data);
 		 ngx_str_t value;
 		 if (ngx_http_arg(r, (u_char *) "appid", 5, &value) == NGX_OK) {
 		 	ngx_cpystrn((u_char *)appid, value.data, value.len);
