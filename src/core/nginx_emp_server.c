bf0934d60c6324afc241de8c3572e5f479f86307
ffcs <myoldman@163.com>
2012-11-23 14:42:54 +0800

    test

--- src/core/nginx_emp_server.c
+++ src/core/nginx_emp_server.c
@@ -590,7 +590,7 @@ int make_request(request_context_t *ctx ,const char * method, char * output_data
         evhttp_connection_free(ctx->connection);  
       
     const char * host = evhttp_uri_get_host(ctx->uri);
-	const char *query_part = evhttp_uri_get_path(ctx->uri);
+	const char *query_part = evhttp_uri_get_query(ctx->uri);
 	printf("query part is %s\n", query_part);
     int port = evhttp_uri_get_port(ctx->uri); 
     ctx->connection = evhttp_connection_base_new(  
