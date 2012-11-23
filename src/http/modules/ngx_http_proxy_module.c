04217a87a898fac8c3d60e92a7b5c7c647bd4246
ffcs <myoldman@163.com>
2012-11-23 14:10:25 +0800

    test

--- src/http/modules/ngx_http_proxy_module.c
+++ src/http/modules/ngx_http_proxy_module.c
@@ -625,7 +625,7 @@ static ngx_path_init_t  ngx_http_proxy_temp_path = {
     ngx_string(NGX_HTTP_PROXY_TEMP_PATH), { 1, 2, 0 }
 };
 
-static void ngx_emp_base64_encode_request_body(ngx_http_request_t *r, char **verify_body, int *verify_body_length) {
+static ngx_int_t ngx_emp_base64_encode_request_body(ngx_http_request_t *r, char **verify_body, int *verify_body_length) {
 	u_char 				*p, *last, *buf;
 	ngx_buf_t			*request_body_buf;
 	ngx_chain_t         *cl;
@@ -699,7 +699,7 @@ static void ngx_emp_base64_encode_request_body(ngx_http_request_t *r, char **ver
 	//	request_args_after_encode.data = ngx_palloc(r->pool, request_args_after_encode.len);
 	//	ngx_encode_base64(&request_args_after_encode, &r->args);
 	//}
-	
+	return NGX_OK;
 }
 
 /* fork from ngx_http_arg.
